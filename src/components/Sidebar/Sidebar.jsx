974171399ab72fc040f9f0c403a108318b5485b1
SSH37 <suwakovic@gmail.com>
2024-12-03 16:48:36 +0100

    Fetch for Sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
 import { fetchUrl } from '../../library'
 import { urlCompanies } from '../../constants/constants'
+import { useState,useEffect } from 'react'
 
 const Sidebar = () => {
-  return (
-    <div>Sidebar</div>
+  const [companies, setCompanies]=useState([])
+
+  useEffect(()=>{fetchUrl(urlCompanies,(res)=>{setCompanies(res)})})
+
+  return (<>
+    <div><h2></h2>
+    <ul>
+    {
+      companies.map((el)=>{
+        return <li>{el.name}</li>
+      })
+    }
+   
     
+    </ul>
+    </div>
+    
+    </>
   )
 }
 
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,4 +1,4 @@
 export const urlCandidates = "localhost:3333/api/candidates"
-export const urlCompanies = "localhost:3333/api/companies"
+export const urlCompanies = "http://localhost:3333/api/companies"
 export const urlReports = "localhost:3333/api/reports"
 export const urlUsers = "localhost:3333/api/users"
