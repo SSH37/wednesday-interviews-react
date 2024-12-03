c262e967cfc8264162e5964173348f1169619f46
pejkanaaaaa <sredo.pejovic@gmail.com>
2024-12-03 17:02:10 +0100

    welcome page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,20 +2,21 @@ import React from "react";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 import Sidebar from "../../components/Sidebar/Sidebar";
-import Card from "../../components/Card/Card"
-import "./HomePage.css"
+import Card from "../../components/Card/Card";
+import "./HomePage.css";
 
 const HomePage = () => {
-  
   //temp
-  let data = [1,2,3] 
+  let data = [1, 2, 3];
   return (
     <>
       <Header />
       <div id="content">
-        <Sidebar/>
+        <Sidebar />
         <div id="candidatesList">
-          {data.map((cardData)=>{return <Card data={cardData}/>})}
+          {data.map((cardData) => {
+            return <Card data={cardData} />;
+          })}
         </div>
       </div>
       <Footer />
