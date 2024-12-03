f9c88602cded4049afa3d1f3424d47d349bb8dec
reroci <reroci3524@angewy.com>
2024-12-03 15:20:40 +0100

    Added url constants, fetch function, footer, initial HomePage setup, etc..

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Card = () => {
+const Card = ( { data }) => {
   return (
     <div>
-      
+      Card{data}
     </div>
   )
 }
--- src/pages/SinglePage/SinglePage.jsx
+++ src/pages/SinglePage/SinglePage.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
 
 const SinglePage = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+};
 
-export default SinglePage
+export default SinglePage;
