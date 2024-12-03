59ddf3563ac06596ca90aebbf757110d423b4ba1
reroci <reroci3524@angewy.com>
2024-12-03 15:34:34 +0100

    fix app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import Header from "./components/Header/Header";
-import "./App.css"
+import "./App.css";
+import { Routes, Route, Navigate } from "react-router";
+import HomePage from "./pages/HomePage/HomePage";
+import SinglePage from "./pages/SinglePage/SinglePage";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <p>123</p>
-    </div>
+    <Routes>
+      <Route path="/home" element={<HomePage />} />
+      <Route path="/candidate/:id" element={<SinglePage />} />
+      <Route path="/*" element={<Navigate to={"/home"} />} />
+    </Routes>
   );
 }
 
