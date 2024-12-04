ac3c859b9a3dec5b245b52d82fe1a6400c471a49
reroci <reroci3524@angewy.com>
2024-12-04 12:06:14 +0100

    Added card navigate, email popup

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from "react";
 import "./Card.css";
 import { fetchUrl } from "../../library";
+import { useNavigate } from "react-router";
 
 const Card = ({ data }) => {
   const [imageUrl, setImageUrl] = useState("");
   // const [imageUrl, setImageUrl] = useState(data.avatar);
   //db gets 403 for avatar urls so fetch random picturess
+  const nav = useNavigate();
+  const [emailShow, setEmailShow] = useState(false);
 
   useEffect(() => {
     fetchUrl("https://randomuser.me/api/?inc=picture&noinfo", (res) => {
@@ -14,20 +17,37 @@ const Card = ({ data }) => {
   }, []);
 
   return (
-    <div className="card">
+    <div
+      className="card"
+      onClick={() => {
+        nav(`/candidate/${data.id}`);
+      }}
+    >
       {/* <img src={data.avatar} alt={data.name} /> */}
       <img src={imageUrl ? imageUrl : ""} alt={data.name} />
 
-      <div>
+      <div style={{ position: "relative" }}>
         <p>{data.name}</p>
-        <p>
-          {data.email.length > 15
+        <p
+          onMouseOver={() => {
+            if (data.email.length > 15) {
+              setEmailShow(true);
+            }
+          }}
+          onMouseLeave={() => {
+            setEmailShow(false);
+          }}
+        >
+          {data.email.length > 20
             ? `${[
                 ...data.email.slice(0, 4),
                 "...",
                 ...data.email.slice(data.email.indexOf("@")),
               ].join("")}`
             : data.email}
+        </p>
+        <p className={`${emailShow ? "cardEmailHovered" : "cardEmailHidden"}`}>
+          {data.email}
         </p>
       </div>
     </div>
