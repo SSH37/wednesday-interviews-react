ffba7b7130536efb96bd0f3c95d596c73cf1b65c
reroci <reroci3524@angewy.com>
2024-12-05 16:02:26 +0100

    Init Report

--- src/components/Report/Report.jsx
+++ src/components/Report/Report.jsx
@@ -1,19 +1,22 @@
-import "./Report.css"
+import "./Report.css";
 import { LuNewspaper } from "react-icons/lu";
-const Report = ({data, setData}) => {
+const Report = ({ data, setData }) => {
   // setData(null) in onClick
+  const reportDate = new Date(data.interviewDate);
 
   console.log(data);
   return (
     <div className="reportModal">
       <div className="reportContent">
-      <button className="closeButton" onClick={() => setData(null)}>×</button>
-        <LuNewspaper style={{
-          marginBottom:"15px"
-        }}/>
-        <p className="reportText">{data.note}</p></div>
+        <button className="closeButton" onClick={() => setData(null)}>
+          ×
+        </button>
+        <LuNewspaper className="reportIcon" />
+        <p className="reportText">{data.note}</p>
+        {`${reportDate.getUTCDay()}.${reportDate.getUTCMonth()}.${reportDate.getUTCFullYear()}`}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Report
+export default Report;
