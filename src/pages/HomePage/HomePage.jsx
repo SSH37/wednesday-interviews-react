f9fddd19d91dadc7caceff26fa823df6d8b26d42
reroci <reroci3524@angewy.com>
2024-12-05 14:54:36 +0100

    Added non bounced search, homepage sub header, changed pixel css to em, added rounded border to cards

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -5,26 +5,48 @@ import Sidebar from "../../components/Sidebar/Sidebar";
 import Card from "../../components/Card/Card";
 import { fetchUrl } from "../../library";
 import { urlCandidates } from "../../constants/constants";
+import { FaSearch } from "react-icons/fa";
 import "./HomePage.css";
 
 const HomePage = () => {
   const [candidates, setCandidates] = useState([]);
+  const [filterParam, setFilterParam] = useState("");
 
   useEffect(() => {
-    fetchUrl(urlCandidates, (res) => {
+    fetchUrl(`${urlCandidates}${filterParam}`, (res) => {
       setCandidates(res);
     });
-  }, []);
+  }, [filterParam]);
 
   return (
     <>
       <Header />
       <div id="content">
         <Sidebar />
-        <div id="candidatesList">
-          {candidates.map((cardData) => {
-            return <Card key={cardData.id} data={cardData} />;
-          })}
+        <div id="candidatesListParent">
+          <div className="candidatesSubHeader">
+            <p>Candidates</p>
+            <div className="searchBox">
+              <FaSearch className="iconHP" />
+              <input
+                className=""
+                type="search"
+                name="search"
+                id="search"
+                onInput={(e) => {
+                  setFilterParam(`?name_like=${e.target.value}`);
+                }}
+              />
+              <label htmlFor="search" id="searchLabel">
+                Search candidates
+              </label>
+            </div>
+          </div>
+          <div id="candidatesList">
+            {candidates.map((cardData) => {
+              return <Card key={cardData.id} data={cardData} />;
+            })}
+          </div>
         </div>
       </div>
       <Footer />
