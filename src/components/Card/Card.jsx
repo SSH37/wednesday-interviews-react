c6a4b9ee494eaa59b2258225f5642bd0ef8a5b41
reroci <reroci3524@angewy.com>
2024-12-10 17:48:41 +0100

    Added tooltip to card, changed readme to include what's used

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import "./Card.css";
 import { fetchUrl } from "../../library";
 import { useNavigate } from "react-router";
+const emailShowDelay = 300;
 
 const Card = ({ data }) => {
   const [imageUrl, setImageUrl] = useState("");
@@ -9,6 +10,9 @@ const Card = ({ data }) => {
   //db gets 403 for avatar urls so fetch random pictures for now
   const nav = useNavigate();
   const [emailShow, setEmailShow] = useState(false);
+  const [emailShowTimestamp, setEmailShowTimestamp] = useState(null);
+  const [emailShowId, setEmailShowId] = useState(null);
+  const [emailPos, setEmailPos] = useState([0, 0]);
 
   useEffect(() => {
     fetchUrl("https://randomuser.me/api/?inc=picture&noinfo", (res) => {
@@ -16,40 +20,67 @@ const Card = ({ data }) => {
     });
   }, []);
 
-  return (
-    <div
-      className="card"
-      onClick={() => {
-        nav(`/candidate/${data.id}`);
-      }}
-    >
-      {/* <img src={data.avatar} alt={data.name} /> */}
-      <img src={imageUrl ? imageUrl : ""} alt={data.name} />
+  useEffect(() => {
+    if (!emailShow) {
+      console.log(emailShow, emailPos);
+      setEmailPos([0, 0]);
+    } else {
+      console.log(emailShow, emailPos);
+      // setEmailPos([mouseEv.clientX, mouseEv.clientY])
+    }
+  }, [emailShow]);
 
-      <div style={{ position: "relative" }}>
-        <p>{data.name}</p>
-        <p
-          onMouseOver={() => {
-            if (data.email.length > 15) {
-              setEmailShow(true);
-            }
-          }}
-          onMouseLeave={() => {
-            setEmailShow(false);
-          }}
-        >
-          {data.email.length > 15
-            ? `${[
-                ...data.email.slice(0, 3),
-                "...",
-                ...data.email.slice(data.email.indexOf("@")),
-              ].join("")}`
-            : data.email}
-        </p>
-        <p className={`${emailShow ? "cardEmailHovered" : "cardEmailHidden"}`}>
-          {data.email}
-        </p>
+  return (
+    <div>
+      <div
+        className="card"
+        onClick={() => {
+          nav(`/candidate/${data.id}`);
+        }}
+        onMouseMove={(e) => {
+          if (!emailShowTimestamp) {
+            console.log("fired email show");
+            setEmailShowTimestamp(Date.now());
+            setEmailShowId(
+              setTimeout(() => {
+                setEmailShow(true);
+              }, emailShowDelay)
+            );
+          } else if (emailShowTimestamp + emailShowDelay < Date.now()) {
+          } else {
+            console.log("Setting email position");
+            console.log(e.clientX, e.clientY);
+            setEmailPos([e.clientX, e.clientY]);
+          }
+        }}
+        onMouseLeave={() => {
+          clearTimeout(emailShowId);
+          setEmailShowTimestamp(null);
+          setEmailShow(false);
+        }}
+      >
+        {/* <img src={data.avatar} alt={data.name} /> */}
+        <img src={imageUrl ? imageUrl : ""} alt={data.name} />
+        <div className="cardInfo">
+          <p>{data.name}</p>
+          <p>{data.education}</p>
+        </div>
       </div>
+      <p
+        style={{
+          left: `${emailPos[0]}px`,
+          top: `${emailPos[1]}px`,
+        }}
+        className={`${emailShow ? "cardEmailHovered" : "cardEmailHidden"}`}
+        onMouseOver={(e) => {
+            setEmailShow(true);
+        }}
+        onMouseLeave={() => {
+          setEmailShow(false);
+        }}
+      >
+        {data.email}
+      </p>
     </div>
   );
 };
